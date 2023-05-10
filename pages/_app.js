333113b32d7a3580c5666346432705df513c0d94
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-10 04:41:05 -0400

    add banner comp :heavy_check_mark:

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { Nav } from "@/components/Nav";
+import { Banner } from "@/components/Banner";
 import { Footer } from "@/components/Footer";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Banner />
       <Nav />
       <Component {...pageProps} />
       <Footer />
