ed09048ddbb5a54ba4e422fe32c8455a0e0c3631
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-10 01:50:34 -0400

    fix arrows bug :lady_beetle:

--- components/Banner.js
+++ components/Banner.js
@@ -27,10 +27,23 @@ export const Banner = () => {
       <div class="overflow-hidden">
         <div class="flex relative max-h-[30rem]">
           <button
-            className="absolute font-bold text-lg top-[35%] mx-4 p-4 opacity-75 rounded-lg bg-gray-100 text-black"
+            className="absolute font-bold hover:opacity-100 text-lg top-[35%] mx-4 p-4 opacity-75 rounded-full bg-gray-100 text-black"
             onClick={handlePrev}
           >
-            &lt;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15.75 19.5L8.25 12l7.5-7.5"
+              />
+            </svg>
           </button>
           <img
             class="w-screen max-h-[30rem]"
@@ -38,10 +51,23 @@ export const Banner = () => {
             alt="Image 1"
           />
           <button
-            className="absolute font-bold text-lg top-[35%] right-0 mx-4 p-4 opacity-75 rounded-lg bg-gray-100 text-black"
+            className="absolute font-bold text-lg top-[35%] right-0 mx-4 p-4 opacity-75 hover:opacity-100 rounded-full bg-gray-100 text-black"
             onClick={handleNext}
           >
-            &gt;
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.25 4.5l7.5 7.5-7.5 7.5"
+              />
+            </svg>
           </button>
         </div>
       </div>
