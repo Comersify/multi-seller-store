db0bf15ee705a7fc847c6f6c7fbbe64a2ab5305e
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-10 03:54:33 -0400

    add auth forms

--- forms/LoginForm.js
+++ forms/LoginForm.js
@@ -1,3 +1,42 @@
+import Link from "next/link";
+import { Input } from "./Input";
+import { Title } from "@/components/Title";
+
 export const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <div class="flex flex-col justify-center items-center h-[45rem]">
+      <Title text="Login" emoji="&#x1F512;" />
+      <form class="w-full mt-4 p-8 max-w-sm border rounded-lg border-gray-200">
+        <div class="mb-4">
+          <Input label="Username" type="text" placeholder="username" />
+        </div>
+        <div class="mb-6">
+          <Input label="Password" type="password" placeholder="Password" />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign In
+          </button>
+          <Link
+            class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </Link>
+        </div>
+      </form>
+      <p className="text-gray-900 mt-4">
+        You don't have an account
+        <Link
+          class="px-2 inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+          href="/signup"
+        >
+          Sign Up
+        </Link>
+      </p>
+    </div>
+  );
 };
--- forms/SignUpForm.js
+++ forms/SignUpForm.js
@@ -1,3 +1,41 @@
+import { Title } from "@/components/Title";
+import Link from "next/link";
+import { Input } from "./Input";
+
 export const SignUpForm = () => {
-  return <div>SignUpForm</div>;
+  return (
+    <div class="flex flex-col justify-center items-center h-[45rem] w-[50rem]">
+      <Title text="Sign Up" />
+      <form class=" mt-4 p-8 border rounded-lg border-gray-200 w-[30rem]">
+        <div class="mb-4 flex justify-between">
+          <Input label="First Name" type="text" />
+          <span className="p-4"></span>
+          <Input label="Last Name" type="text" />
+        </div>
+        <div class="mb-4">
+          <Input label="Email" type="email" />
+        </div>
+        <div class="mb-4">
+          <Input type="password" label="Password" />
+        </div>
+        <div class="mb-6">
+          <Input type="password" label="Confirm Password" />
+        </div>
+        <div class="flex w-full justify-center">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+      <Link
+        className="mt-4 inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+        href="/login"
+      >
+        Already have an account?
+      </Link>
+    </div>
+  );
 };
