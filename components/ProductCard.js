5db9d8d40dd6922159ee7826810964820380b49c
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-11 03:03:09 -0400

    create disocunt tag comp :heavy_check_mark:

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,5 +1,13 @@
 import { Stars } from "./Stars";
 
+const DiscountTag = ({ value }) => {
+  return (
+    <span class="absolute top-1 right-1 bg-red-500 rounded-full px-3 py-1 text-sm font-semibold text-white">
+      {value}% off
+    </span>
+  );
+};
+
 export const ProductCard = () => {
   return (
     <div class="w-52 border border-gray-200 rounded relative overflow-hidden hover:shadow-sm">
@@ -8,9 +16,7 @@ export const ProductCard = () => {
         src="https://via.placeholder.com/150"
         alt="Product Image"
       />
-      <span class="absolute top-1 right-1 bg-red-500 rounded-full px-3 py-1 text-sm font-semibold text-white">
-        10% off
-      </span>
+      <DiscountTag value={10} />
       <div class="px-4 py-2">
         <h2 class="font-bold text-lg text-gray-900">Product Title</h2>
         <div class="flex justify-between items-center mt-1">
