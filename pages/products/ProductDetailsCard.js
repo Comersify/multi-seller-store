ee47c7c04fcc9bc03efc7d5221147f31d8a7d94c
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-13 11:48:23 -0400

    add save button :heart:

--- pages/products/ProductDetailsCard.js
+++ pages/products/ProductDetailsCard.js
@@ -1,4 +1,6 @@
 import { Stars } from "@/components/Stars";
+import { SaveButton } from "@/components/shared/Buttons";
+import { useState } from "react";
 
 const ProductDescription = () => {
   return (
@@ -150,10 +152,11 @@ export const ProductChoices = () => {
 export const ProductDetailsCard = () => {
   return (
     <div className="mx-auto w-90 px-4 pb-8 pt-10 col-span-2 lg:col-span-3 sm:px-6 lg:grid lg:grid-cols-3 lg:grid-rows-[auto,auto,1fr] lg:gap-x-8 lg:px-8 lg:pb-10 lg:pt-16">
-      <div className="lg:col-span-2 lg:border-r lg:border-gray-200 lg:pr-8">
+      <div className="lg:col-span-2 lg:border-r flex items-center justify-between lg:border-gray-200 lg:pr-8">
         <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
           Basic Tee 6-Pack
         </h1>
+        <SaveButton />
       </div>
 
       {/* <!-- Options --> */}
