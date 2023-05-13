8126a67ce6f0cf7d94ad32c7c34801568d1e8a2a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-13 06:29:11 -0400

    add checkout page :heavy_check_mark:

---
+++ pages/checkout/PaymentCard.jsx
@@ -0,0 +1,42 @@
+export const PaymentCard = ({ active }) => {
+  return (
+    <div
+      className={`w-full cursor-pointer select-none bg-white flex shadow-sm p-6 max-[741px]:flex-col max-[741px]:w-[300px] justify-between rounded-lg focus:outline-none ${
+        active
+          ? "ring-2 ring-orange-500 ring-offset-orange-200"
+          : "border border-gray-300"
+      }`}
+    >
+      <div className="flex flex-col mb-4 mr-4">
+        <label className="font-bold mb-2" for="full-name">
+          Full Name
+        </label>
+        <span id="full-name">John Doe</span>
+      </div>
+      <div className="flex flex-col mb-4 mr-4">
+        <label className="font-bold mb-2" for="address">
+          Address
+        </label>
+        <span id="address">123 Main Street</span>
+      </div>
+      <div className="flex flex-col mb-4 mr-4">
+        <label className="font-bold mb-2" for="phone-number">
+          Phone Number
+        </label>
+        <span id="phone-number">(123) 456-7890</span>
+      </div>
+      <div className="flex flex-col mb-4 mr-4">
+        <label className="font-bold mb-2" for="postal-code">
+          Postal Code
+        </label>
+        <span id="postal-code">A1B 2C3</span>
+      </div>
+      <div className="flex flex-col mb-4 mr-4">
+        <label className="font-bold mb-2" for="card-number">
+          Visa Card Number
+        </label>
+        <span id="card-number">**** **** **** 1234</span>
+      </div>
+    </div>
+  );
+};
---
+++ pages/checkout/index.jsx
@@ -0,0 +1,25 @@
+import Head from "next/head";
+import { PaymentCard } from "./PaymentCard";
+import { PaymentForm } from "./PaymentForm";
+
+function Checkout() {
+  return (
+    <>
+      <Head>
+        <title>Checkout</title>
+      </Head>
+      <main className="p-10 w-full text-gray-900">
+        <h1 className="text-4xl mb-4">Pay Now</h1>
+        <div className="flex flex-wrap justify-center items-center">
+          <div className="mr-6 flex flex-col gap-6 max-sm:mr-0 mb-5">
+            <PaymentCard active />
+            <PaymentCard />
+            <PaymentCard />
+          </div>
+          <PaymentForm />
+        </div>
+      </main>
+    </>
+  );
+}
+export default Checkout;
