29a5cffa9e816badd7f08112f7a96de06f570655
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-13 06:28:41 -0400

    add cart page :heavy_check_mark:

---
+++ pages/cart/CheckoutCard.jsx
@@ -0,0 +1,43 @@
+const ApplyCoupon = () => {
+  return (
+    <div className="flex items-center justify-between mb-2 py-4 border-b-2 border-gray-400">
+      <p className="text-lg font-bold text-gray-500 py-1 mr-2">Coupon:</p>
+      <input type="text" className="py-1 border border-gray-300 rounded-md" />
+      <button className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 ml-2 px-4 py-1 text-base font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+        Apply
+      </button>
+    </div>
+  );
+};
+
+export const CheckoutCard = () => {
+  return (
+    <div className="relative">
+      <div className="fixed max-[1150px]:relative bg-gray-100 rounded-md p-4">
+        <ApplyCoupon />
+        <div className="flex items-center justify-between mb-2">
+          <p className="text-lg font-bold text-gray-500">SubTotal:</p>
+          <p className="text-lg font-bold">$300</p>
+        </div>
+        <div className="flex items-center justify-between mb-2">
+          <p className="text-lg font-bold text-gray-500">Discount:</p>
+          <p className="text-lg font-bold">$300</p>
+        </div>
+        <div className="flex items-center justify-between mb-2 pb-2 border-0 border-b-2 border-b-gray-400">
+          <p className="text-lg font-bold text-gray-500">Shipping:</p>
+          <p className="text-lg font-bold">$300</p>
+        </div>
+        <div className="flex items-center justify-between mb-2 overflow-hidden">
+          <p className="font-bold text-lg">Total:</p>
+          <p className="text-lg font-bold">$300</p>
+        </div>
+        <a
+          href="checkout"
+          className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        >
+          Checkout
+        </a>
+      </div>
+    </div>
+  );
+};
--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,3 +1,26 @@
+import Head from "next/head";
+import { ProductItem } from "@/components/ProductCard";
+import { CheckoutCard } from "./CheckoutCard";
+
 export default function Cart() {
-  return <div>Cart</div>;
+  return (
+    <>
+      <Head>
+        <title>My Cart</title>
+      </Head>
+      <main className="text-gray-900 md:px-16 px-4 py-10">
+        <h1 className="text-4xl font-bold py-6 max-sm:px-2">Shopping Cart</h1>
+        <div className="py-4 flex flex-wrap gap-y-2 px-0 justify-start">
+          <div className="max-sm:flex max-sm:flex-wrap max-sm:gap-y-2 max-sm:justify-center">
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+          </div>
+          <CheckoutCard />
+        </div>
+      </main>
+    </>
+  );
 }
