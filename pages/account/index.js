e2d86f4883c7ed0101c25f04f5e2d6f991f3fa15
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-19 14:46:30 -0400

    add wish list page :heavy_plus_sign:

--- pages/account/index.js
+++ pages/account/index.js
@@ -1,3 +1,24 @@
+import { ProductCard } from "@/components/ProductCard";
+import { Title } from "@/components/shared/Title";
+import Head from "next/head";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <>
+      <Head>Profile</Head>
+      <main className="min-h-[68vh] px-16 pt-8">
+        <Title text="Wish List" />
+        <div className="mt-8 flex flex-wrap gap-8">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </main>
+    </>
+  );
 }
