ca744c42e6546b6e60e3cf3a9515af08ad61429a
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-19 14:49:22 -0400

    :fire:

---
+++ pages/account/settings/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Settings() {
+  return (
+    <>
+      <Head>Settings</Head>
+      <main className="h-[70vh] flex items-center justify-center"></main>
+    </>
+  );
+}
