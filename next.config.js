d2cbe1f3c68c9a8657f49031620fa4d0c6d2dc3c
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-29 01:02:11 -0400

    add test images to domains :star:

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["via.placeholder.com", "picsum.photos"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
