8969f0b0a44a04c74ae3f8415614ab3985235168
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-29 02:13:28 -0400

    create signup api logic :heavy_check_mark:

--- pages/signup/SignUpForm.js
+++ pages/signup/SignUpForm.js
@@ -1,33 +1,60 @@
 import { Title } from "@/components/shared/Title";
 import Link from "next/link";
 import { Input } from "@/forms/Input";
+import { useSignup } from "@/api/api";
+import { Button } from "@/components/shared/Buttons";
 
 export const SignUpForm = () => {
+  const { form, setForm, handleSubmit } = useSignup();
   return (
     <div className="flex p-4 flex-col justify-center items-center h-[40rem] ">
       <Title text="Sign Up" />
       <form className=" mt-4 py-8 px-16  border rounded-lg border-gray-200 w-[30rem]">
         <div className="mb-4 block md:flex justify-between">
-          <Input label="First Name" type="text" />
+          <Input
+            value={form.firstName}
+            onChange={(e) => setForm({ ...form, firstName: e.target.value })}
+            label="First Name"
+            type="text"
+          />
           <span className="px-4"></span>
-          <Input label="Last Name" type="text" />
+          <Input
+            value={form.lastName}
+            onChange={(e) => setForm({ ...form, lastName: e.target.value })}
+            label="Last Name"
+            type="text"
+          />
         </div>
         <div className="mb-4">
-          <Input label="Email" type="email" />
+          <Input
+            value={form.email}
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+            label="Email"
+            type="email"
+          />
         </div>
         <div className="mb-4">
-          <Input type="password" label="Password" />
+          <Input
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+            type="password"
+            label="Password"
+          />
         </div>
         <div className="mb-6">
-          <Input type="password" label="Confirm Password" />
+          <Input
+            value={form.passwordConfermation}
+            onChange={(e) =>
+              setForm({ ...form, passwordConfermation: e.target.value })
+            }
+            type="password"
+            label="Confirm Password"
+          />
         </div>
         <div className="flex w-full justify-center">
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="button"
-          >
+          <Button px="12" onClick={(e) => handleSubmit(e)}>
             Sign Up
-          </button>
+          </Button>
         </div>
       </form>
       <Link
