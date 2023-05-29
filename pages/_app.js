51b0fce739995ef662e8f3071503b3aa04af5f07
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-29 01:07:31 -0400

    wrap the context

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import { Nav } from "@/components/Nav";
 import { Banner } from "@/components/Banner";
 import { Footer } from "@/components/Footer";
 import "@/styles/globals.css";
+import { ContextProvider } from "@/context/contextProvider";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <ContextProvider>
       <Banner />
       <Nav />
       <Component {...pageProps} />
       <Footer />
-    </>
+    </ContextProvider>
   );
 }
