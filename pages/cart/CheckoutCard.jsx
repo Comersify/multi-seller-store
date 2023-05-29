23f5c83dca58c4d2782c8411ae2d42164895f3b6
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-29 08:05:04 -0400

    add checkout logic :heavy_plus_sign:

--- pages/cart/CheckoutCard.jsx
+++ pages/cart/CheckoutCard.jsx
@@ -1,37 +1,65 @@
+import { useGetCouponValue } from "@/api/api";
 import Link from "next/link";
 
-const ApplyCoupon = () => {
+const ApplyCoupon = ({ handleApply, setCoupon, coupon }) => {
   return (
     <div className="flex items-center justify-between mb-2 py-4 border-b-2 border-gray-400">
       <p className="text-lg font-bold text-gray-500 py-1 mr-2">Coupon:</p>
-      <input type="text" className="py-1 border border-gray-300 rounded-md" />
-      <button className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 ml-2 px-4 py-1 text-base font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+      <input
+        onChange={(e) => setCoupon(e.target.value)}
+        type="text"
+        value={coupon}
+        className="py-1 px-2 border border-gray-300 rounded-md"
+      />
+      <button
+        type="submit"
+        onClick={(e) => handleApply(e)}
+        className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 ml-2 px-4 py-1 text-base font-bold text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+      >
         Apply
       </button>
     </div>
   );
 };
 
-export const CheckoutCard = () => {
+export const CheckoutCard = ({ subTotal, discount, shipping }) => {
+  const { setCoupon, handleApply, coupon, coupons, values } =
+    useGetCouponValue();
   return (
     <div className="relative">
       <div className="fixed max-[1150px]:relative bg-gray-100 rounded-md p-4">
-        <ApplyCoupon />
+        <ApplyCoupon
+          handleApply={handleApply}
+          setCoupon={setCoupon}
+          coupon={coupon}
+        />
+        <div className="flex items-center justify-between mb-2">
+          {coupons.map((c) => (
+            <>
+              <p className="text-lg font-bold text-gray-500">
+                Coupon [{c.code}]:
+              </p>
+              <p className="text-lg font-bold">${c.value}</p>
+            </>
+          ))}
+        </div>
         <div className="flex items-center justify-between mb-2">
           <p className="text-lg font-bold text-gray-500">SubTotal:</p>
-          <p className="text-lg font-bold">$300</p>
+          <p className="text-lg font-bold">${subTotal}</p>
         </div>
         <div className="flex items-center justify-between mb-2">
           <p className="text-lg font-bold text-gray-500">Discount:</p>
-          <p className="text-lg font-bold">$300</p>
+          <p className="text-lg font-bold">${discount}</p>
         </div>
         <div className="flex items-center justify-between mb-2 pb-2 border-0 border-b-2 border-b-gray-400">
           <p className="text-lg font-bold text-gray-500">Shipping:</p>
-          <p className="text-lg font-bold">$300</p>
+          <p className="text-lg font-bold">${shipping}</p>
         </div>
         <div className="flex items-center justify-between mb-2 overflow-hidden">
           <p className="font-bold text-lg">Total:</p>
-          <p className="text-lg font-bold">$300</p>
+          <p className="text-lg font-bold">
+            ${subTotal - discount + shipping - values}
+          </p>
         </div>
         <Link
           href="checkout"
