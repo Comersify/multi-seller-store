34bee1af9935c1316e221cfcfaa443301c4b54cb
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-29 01:05:31 -0400

    create review api :heavy_check_mark:

--- components/ReviewCards.js
+++ components/ReviewCards.js
@@ -1,14 +1,19 @@
-export const HomeReviewCard = () => {
+import Image from "next/image";
+
+export const HomeReviewCard = ({ image, review }) => {
   return (
-    <div className="max-w-[20rem] rounded-lg border relative border-gray-200 shadow-sm ">
-      <img
-        className="w-20 h-20 rounded-full absolute -top-8 -left-5 "
-        src="https://via.placeholder.com/150"
-      />
-      <p className="text-black text-ms pt-10 pl-10 pr-4 pb-4">
-        lorem ipsum dollar bmlkflbfl ckd s jMKJ SKJB JVBKM kmJ MK K SK kSH KMHk
-        kH MKH kh MKH MKH QKH{" "}
-      </p>
+    <div className="overflow-hidden py-10">
+      <div className="w-[20rem] rounded-lg border relative border-gray-200 shadow-sm ">
+        <Image
+          className="rounded-full absolute -top-10 left-[35%]"
+          width={90}
+          height={90}
+          src={image}
+        />
+        <p className="text-black text-ms pt-14 text-center px-4 pb-4 ">
+          {review}
+        </p>
+      </div>
     </div>
   );
 };
