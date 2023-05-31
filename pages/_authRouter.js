f06c4f4cdca7df236929a07077e3132d9885eca0
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-05-31 11:43:31 -0400

    use refresh token hook :alien:

--- pages/_authRouter.js
+++ pages/_authRouter.js
@@ -1,37 +1,11 @@
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { useStateContext } from "@/context/contextProvider";
-import { refreshToken } from "@/api/auth";
+import { useRefresh } from "@/api/auth";
 
 const useWithAuth = (WrappedComponent) => {
   return (props) => {
-    const router = useRouter();
-    const { token, handleToken, handleNotification } = useStateContext();
+    const { token } = useStateContext();
     if (token) return <WrappedComponent {...props} />;
-
-    useEffect(() => {
-      let access;
-      let data;
-      async function refresh(){
-        if (typeof window !== "undefined") {
-          access = localStorage.getItem("refresh");
-          if (access) {
-            data = await refreshToken({refresh: access});
-            if (data?.type == "success") {
-              handleToken(data);
-            } else {
-              if (data?.type == "error") handleNotification(data);
-              router.replace("/login");
-            }
-          } else {
-            router.replace("/login");
-          }
-      }
-      }
-      refresh()
-    }, []);
-
-    // Render the wrapped component if the user is authenticated
+    useRefresh();
     return token ? <WrappedComponent {...props} /> : null;
   };
 };
