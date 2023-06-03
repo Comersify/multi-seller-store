c96ab1f8c6707633339ee56fee16472493c3c723
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:04:42 -0400

    add get product details logic :heavy_plus_sign:

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -7,52 +7,46 @@ import { useRouter } from "next/router";
 import { useGetProducts } from "@/api/product";
 import { useGetReviews } from "@/api/review";
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: true,
-  };
-}
-
-export async function getStaticProps({ params }) {
-  const postData = [];
-
-  return {
-    props: {
-      product: postData,
-      reviews: postData,
-    },
-  };
-}
 
 export default function ProductDetails() {
   const router = useRouter();
   const { id } = router.query;
-  const { product } = useGetProducts({ filter: `id/${id}/` });
-  const { reviews } = useGetReviews();
+  if (!id) return <p>Loading</p>
+  const { products: product } = useGetProducts({ filter: `id/${id}/` });
+  const  {reviews}  = useGetReviews(id);
+  if (!reviews) return <p>Loading</p>
+
   return (
     <>
       <Head>
-        <title>{product?.title || "title"}</title>
+        <title>{product?.title}</title>
       </Head>
       <main className="px-6 md:px-20 lg:px-34">
         <div className="px-6 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-5 lg:px-8">
           <Gallery images={product?.images} />
           <ProductDetailsCard
+            id={product?.id}
             price={product?.price}
-            stars={product?.stars}
-            starsCount={product?.starsCount}
+            stars={product?.reviews_avg}
+            starsCount={product?.reviews}
             description={product?.description}
-            discount={product?.discount}
+            discount={product?.discount_value}
             packs={product?.packs}
-            title={product?.title || "Products Title"}
+            title={product?.title}
           />
         </div>
-        {/*      <div className="md:flex sm:block mb-12 items-center justify-center">
-          <ShippingCard />
-          <ShippingCard />
-        </div> */}
-        <ReviewCard reviews={reviews} />
+        {/*      
+          <div className="md:flex sm:block mb-12 items-center justify-center">
+            <ShippingCard />
+            <ShippingCard />
+          </div> 
+        */}
+        {reviews?.reviews?.length > 0 ? 
+        <ReviewCard reviews={reviews}  />
+        : 
+        <p className="text-xl text-gray-400 font-bold text-center border rounded-md py-20 mb-20">Not Reviewed yet ..</p>
+        }
+        
       </main>
     </>
   );
