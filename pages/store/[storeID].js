b31dcbc9c9e66f496829700d8d9a7d8154aa2084
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:09:56 -0400

    add get store details logic :heavy_plus_sign:

--- pages/store/[storeID].js
+++ pages/store/[storeID].js
@@ -3,17 +3,17 @@ import { ProductCard } from "@/components/ProductCard";
 import { Star } from "@/components/Stars";
 import { OrderIcon } from "@/components/shared/Icons";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/router";
 
 export const Cover = ({ cover }) => {
   return (
     <div className="mb-4">
-      <Image
+      <img
         width={1000}
         height={1000}
-        src={cover || "https://picsum.photos/id/1018/1200/500"}
+        src={"http://127.0.0.1:8000" + cover}
         alt="Store cover"
-        className="w-full rounded-lg"
+        className="w-full max-h-[20rem] rounded-lg"
       />
     </div>
   );
@@ -22,34 +22,34 @@ export const Cover = ({ cover }) => {
 export const StoreHeader = ({ name, logo }) => {
   return (
     <div className="flex items-center mb-4">
-      <Image
+      <img
         width={40}
         height={40}
-        src={logo || "https://picsum.photos/id/1018/1200/500"}
+        src={"http://127.0.0.1:8000" + logo}
         alt="Store logo"
         className="w-16 h-16 rounded-full mr-4"
       />
-      <h1 className="text-lg font-medium">{name || "Store Name"}</h1>
+      <h1 className="text-lg font-medium">{name}</h1>
     </div>
   );
 };
 
-export const StoreActivities = ({ stores }) => {
+export const StoreActivities = ({ reviewsAvg, orders, reviews }) => {
   return (
     <div className="flex items-center mb-4">
       <div className="mr-4 flex items-center">
         <span className="text-lg flex items-center font-medium">
-          {stores.reviews || 4.5}
-          <Star active />
+          {reviewsAvg}
+          <Star active={reviewsAvg > 0} />
         </span>
         <p className="text-sm text-gray-500">
-          {stores.reviews || "124 ratings"}
+          {reviews + " ratings"}
         </p>
       </div>
       <div className="mr-4 flex items-center">
         <OrderIcon />
         <p className="text-sm ml-2 text-gray-500">
-          {stores.orders || "6,789 orders"}
+          {orders + " orders"}
         </p>
       </div>
     </div>
@@ -61,43 +61,39 @@ export const StoreDetails = ({ description }) => {
     <>
       <p className="text-sm">Expected shipping time: 2-4 business days</p>
       <p className="text-sm mt-4">
-        {description ||
-          `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at eros non
-        nunc rhoncus posuere. Nunc vel lorem tincidunt, aliquam magna a,
-        pulvinar nisi. Aliquam at justo vitae est vestibulum eleifend. Donec
-        commodo, ipsum eu scelerisque maximus, arcu mi volutpat tellus, at
-        tristique orci lacus in mauris. Aenean tincidunt sapien et venenatis
-        interdum. Nulla facilisi.`}
+        {description}
       </p>
     </>
   );
 };
 
 function Store() {
-  const { stores } = useGetStores({ filter: "id/1/" });
+  const router = useRouter()
+  const {storeID} = router.query
+  if (!storeID)return <p>Loading</p>
+  const { stores: store } = useGetStores({ filter: `id/${storeID}/` });
   return (
     <section className=" text-gray-900 w-full p-6 flex flex-col justify-center items-center">
-      <div className="max-w-6xl">
-        <div className="bg-white  w-full  h-full rounded-lg border border-gray-200 p-4">
-          <Cover cover={stores.cover} />
-          <StoreHeader logo={stores.logo} name={stores.name} />
-          <StoreActivities stores={stores} />
-          <StoreDetails description={stores.description} />
+      <div className="w-full">
+        <div className="bg-white  w-full  max-h-[40rem] rounded-lg border border-gray-200 p-4">
+          <Cover cover={store.cover} />
+          <StoreHeader logo={store.logo} name={store.name} />
+          <StoreActivities reviews={store.reviews} reviewsAvg={store.reviews_avg} orders={store.orders}   />
+          <StoreDetails description={store.description} />
         </div>
         <div className="flex flex-wrap gap-4 justify-center pt-8">
-          {stores?.products?.map(() => {
-            <ProductCard />;
-          })}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {store?.products?.map((product) => (
+            <ProductCard 
+              key={product.id}
+              orders={product.orders}
+              id={product.id}
+              title={product.title}
+              rating={product.reviews}
+              price={product.price}
+              image={product.image}
+              discount={product.discount_value}
+            />
+          ))}
         </div>
       </div>
     </section>
