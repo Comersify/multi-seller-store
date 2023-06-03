69fd6758217d03c3a7210f2b22b4b6df49ad0c07
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-03 17:00:16 -0400

    fix async bug :lady_beetle:

--- api/category.js
+++ api/category.js
@@ -6,12 +6,10 @@ export const useGetCategories = ({filter}) => {
   const [categories, setCategories] = useState([]);
   const { handleNotification } = useStateContext();
   useEffect(() => {
-    async function getCategories(){
-      return useGET(`categories/${filter || ""}`);
-    }
-    const res = getCategories()
-    if (res?.type == "error") handleNotification(res);
-    if (res?.typ == "success") setCategories(res?.data);
-  }, []);
+       useGET(`categories/${filter || ""}`).then((res) =>{
+         if (res?.type == "error") handleNotification(res);
+         if (res?.type == "success") setCategories(res?.data);
+        });
+      }, []);
   return { categories };
 };
