0d510230cb017036dd4eaa386ac15f25ffb17709
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-04 14:55:44 -0400

    pass data to comps & delete test comps :heavy_minus_sign:

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,87 +1,60 @@
 import Head from "next/head";
 import { ProductItem } from "@/components/ProductCard";
 import { CheckoutCard } from "./CheckoutCard";
-import { useState } from "react";
 import useWithAuth from "../_authRouter";
 import { useCart } from "@/api/cart";
-
+import { discountCalc } from "../utils";
 function Cart() {
   const { products, handleUpdate, handleDelete } = useCart();
-  const [counter, setCounter] = useState({
-    subTotal: 0,
-    discount: 0,
-    shipping: 0,
-  });
+  if (products.length <= 0) return <p>Loading</p>;
   return (
     <>
       <Head>
         <title>My Cart</title>
       </Head>
-      <main className="text-gray-900 md:px-16 px-4 py-10">
-        <h1 className="text-4xl font-bold py-6 max-sm:px-2">Shopping Cart</h1>
+      <main className="text-gray-900 md:px-16 py-10">
+        <h1 className="text-4xl font-bold py-6 px-4 max-sm:px-2">
+          Shopping Cart
+        </h1>
         <div className="py-4 flex flex-wrap gap-y-2 px-0 justify-start">
-          <div className="max-sm:flex max-sm:flex-wrap max-sm:gap-y-2 max-sm:justify-center">
-            {products.map((product) => {
-              setCounter({
-                subTotal: counter.subTotal + product.price * product.quantity,
-                discount:
-                  counter.subTotal +
-                  product.price -
-                  (product.discount * product.price) / 100,
-                shipping: counter.shipping,
-              });
+          <div className="max-sm:flex max-sm:flex-wrap max-sm:gap-y-1 max-sm:justify-start">
+            {products?.orders?.map((order) => {
               return (
                 <ProductItem
+                  id={order.id}
+                  productID={order.product__id}
                   handleUpdate={handleUpdate}
                   handleDelete={handleDelete}
-                  image={product.image}
-                  name={product.name}
-                  num={product.quantity}
+                  image={order.product__image}
+                  name={order.product__title}
+                  num={order.quantity}
+                  discount={order.product__discount}
+                  pack={order.pack__title}
                   price={
-                    product.price - (product.discount * product.price) / 100
+                    order.product__price -
+                    discountCalc(order.product__price, order.product__discount)
                   }
                 />
               );
             })}
-            <ProductItem
-              handleUpdate={handleUpdate}
-              handleDelete={handleDelete}
-              name={"Product Name"}
-              discount={10}
-              num={1}
-              shipping={200}
-              price={20}
-              image={"https://via.placeholder.com/450"}
-            />
-            <ProductItem
-              handleUpdate={handleUpdate}
-              handleDelete={handleDelete}
-              name={"Product Name"}
-              discount={10}
-              num={1}
-              price={20}
-              image={"https://via.placeholder.com/450"}
-            />
-            <ProductItem
-              handleUpdate={handleUpdate}
-              handleDelete={handleDelete}
-              name={"Product Name"}
-              discount={10}
-              num={1}
-              price={20}
-              image={"https://via.placeholder.com/450"}
-            />
-            <ProductItem
-              handleUpdate={handleUpdate}
-              handleDelete={handleDelete}
-              name={"Product Name"}
-              discount={10}
-              num={1}
-              price={20}
-              image={"https://via.placeholder.com/450"}
-            />
           </div>
-          <CheckoutCard {...counter} />
+          <CheckoutCard
+            usedCoupons={products?.coupons}
+            discount={products?.orders?.reduce(
+              (accumulator, currentObject) =>
+                accumulator +
+                discountCalc(
+                  currentObject.product__price,
+                  currentObject.product__discount
+                ),
+              0
+            )}
+            subTotal={products?.orders?.reduce(
+              (accumulator, currentObject) =>
+                accumulator + currentObject.product__price,
+              0
+            )}
+          />
         </div>
       </main>
     </>
