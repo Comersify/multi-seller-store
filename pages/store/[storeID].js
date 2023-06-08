5d378b69020126c2358050509cdebb1161378ab7
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-06-08 16:52:50 -0400

    :lady_beetle:

--- pages/store/[storeID].js
+++ pages/store/[storeID].js
@@ -1,6 +1,6 @@
 import { useGetStores } from "@/api/store";
 import { ProductCard } from "@/components/ProductCard";
-import { Star } from "@/components/Stars";
+import { Stars } from "@/components/Stars";
 import { OrderIcon } from "@/components/shared/Icons";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -38,17 +38,22 @@ export const StoreActivities = ({ reviewsAvg, orders, reviews }) => {
   return (
     <div className="flex items-center mb-4">
       <div className="mr-4 flex items-center">
-        <span className="text-lg flex items-center font-medium">
-          {reviewsAvg}
-          <Star active={reviewsAvg > 0} />
-        </span>
-        <p className="text-sm text-gray-500">
-          {reviews + " ratings"}
-        </p>
+        {reviewsAvg > 0 ? (
+          <span className="text-lg flex items-center font-medium">
+            <Stars num={reviewsAvg} />
+
+            <p className="text-sm text-gray-500 mx-2 font-bold">
+              {reviews + " rating"}
+              {reviews > 1 && "s"}
+            </p>
+          </span>
+        ) : (
+          <p className="text-gray-500 text-sm font-bold">Not reviewed yet</p>
+        )}
       </div>
       <div className="mr-4 flex items-center">
         <OrderIcon />
-        <p className="text-sm ml-2 text-gray-500">
+        <p className="text-sm ml-2 text-gray-500 font-bold">
           {orders + " orders"}
         </p>
       </div>
@@ -60,17 +65,15 @@ export const StoreDetails = ({ description }) => {
   return (
     <>
       <p className="text-sm">Expected shipping time: 2-4 business days</p>
-      <p className="text-sm mt-4">
-        {description}
-      </p>
+      <p className="text-sm mt-4">{description}</p>
     </>
   );
 };
 
 function Store() {
-  const router = useRouter()
-  const {storeID} = router.query
-  if (!storeID)return <p>Loading</p>
+  const router = useRouter();
+  const { storeID } = router.query;
+  if (!storeID) return <p>Loading</p>;
   const { stores: store } = useGetStores({ filter: `id/${storeID}/` });
   return (
     <section className=" text-gray-900 w-full p-6 flex flex-col justify-center items-center">
@@ -78,12 +81,16 @@ function Store() {
         <div className="bg-white  w-full  max-h-[40rem] rounded-lg border border-gray-200 p-4">
           <Cover cover={store.cover} />
           <StoreHeader logo={store.logo} name={store.name} />
-          <StoreActivities reviews={store.reviews} reviewsAvg={store.reviews_avg} orders={store.orders}   />
+          <StoreActivities
+            reviews={store.reviews}
+            reviewsAvg={store.reviews_avg}
+            orders={store.orders}
+          />
           <StoreDetails description={store.description} />
         </div>
         <div className="flex flex-wrap gap-4 justify-center pt-8">
           {store?.products?.map((product) => (
-            <ProductCard 
+            <ProductCard
               key={product.id}
               orders={product.orders}
               id={product.id}
