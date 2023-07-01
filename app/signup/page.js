d877311c6cb9f936ebc0ed13165c8d30e9203cf3
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-01 14:46:12 -0400

    :heavy_minus_sign: delete unused code

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,3 +1,4 @@
+"use client";
 import Head from "next/head";
 import { SignUpForm } from "@/forms/SignUpForm";
 
@@ -5,8 +6,10 @@ export default function SignUp() {
   return (
     <>
       <Head>
-        <title>Sign Up</title>
-        <meta name="google-signin-client_id" content="626908472574-pkrn39o1ded5r8gi2guckphcnpimn4f1.apps.googleusercontent.com"></meta>
+        <meta
+          name="google-signin-client_id"
+          content="626908472574-pkrn39o1ded5r8gi2guckphcnpimn4f1.apps.googleusercontent.com"
+        ></meta>
       </Head>
       <SignUpForm />
     </>
