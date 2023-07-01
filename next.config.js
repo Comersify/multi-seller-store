d9775ddb9e1b8c7b4cd7e13b4cc27a2bcb753c3f
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-01 14:45:27 -0400

    disable linter

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["via.placeholder.com", "picsum.photos", "127.0.0.1"],
