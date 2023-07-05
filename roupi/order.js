088e283ac0df8f1d14158980b9eba26d23dae4e2
saadaoui-salah <salahsaadaoui8@gmail.com>
2023-07-04 23:02:50 -0400

    :heavy_plus_sign: add order validation

--- roupi/order.js
+++ roupi/order.js
@@ -23,7 +23,34 @@ export const useCreateOrder = () => {
   const { handleNotification, token, trackID } = useStateContext();
   const submit = (e, data) => {
     e.preventDefault();
-
+    if (!data.fullName) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your full name",
+      });
+      return;
+    }
+    if (!data.address) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your address",
+      });
+      return;
+    }
+    if (!data.phoneNumber) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your phone number",
+      });
+      return;
+    }
+    if (!data.postalCode) {
+      handleNotification({
+        type: "error",
+        message: "Please enter your postal code",
+      });
+      return;
+    }
     usePOST("order/create/", {
       data: data,
       headers: {
